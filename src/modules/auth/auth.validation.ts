c5d73da9681383b52d6b19fc2c1ee70efe61ae10
hasibmuhammaad <hasibmu99@gmail.com>
2026-09-17 21:09:58 +0600

    feat: implement signin functionality with JWT authentication and validation

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -7,3 +7,8 @@ export const signupSchema = z.object({
   phone: z.string().min(10, "Phone number must be at least 10 characters"),
   role: z.enum(["admin", "customer"]).default("customer"),
 });
+
+export const signinSchema = z.object({
+  email: z.email("Invalid email address").toLowerCase(),
+  password: z.string().min(1, "Password is required"),
+});
